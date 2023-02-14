56b0a76bb57237bc30866cf60d3429fae13a4ab0
chao <1019904629@qq.com>
2023-02-14 15:32:06 +0800

    JS decoupling

--- Draw-rect/script.js
+++ Draw-rect/script.js
@@ -1,15 +1,32 @@
-document.addEventListener('mousedown', e => {
-  let rect = document.createElement('div');
-  rect.classList.add('rect');
-  document.body.appendChild(rect);
-  let mousedownX = e.clientX;
-  let mousedownY = e.clientY;
-  rect.style.left = mousedownX + 'px';
-  rect.style.top = mousedownY + 'px';
-  document.onmousemove = e => {
-    rect.style.transform = `scale(${e.clientX - mousedownX}, ${e.clientY - mousedownY})`;
-  };
-});
-document.addEventListener('mouseup', () => {
-  document.onmousemove = null;
-});
+(function () {
+  let callee = void 0;
+  document.addEventListener('mousedown', e => {
+    // 创建矩形
+    const rect = createRectInBody();
+    let mousedownX = e.clientX;
+    let mousedownY = e.clientY;
+    ensureInitialPosition(rect, mousedownX, mousedownY);
+    document.addEventListener('mousemove', function (e) {
+      // 将callee赋值当前函数
+      callee = arguments.callee;
+      rect.style.transform = `scale(${e.clientX - mousedownX}, ${e.clientY - mousedownY})`;
+    });
+  });
+  document.addEventListener('mouseup', () => {
+    document.removeEventListener('mousemove', callee);
+  });
+
+  // 创建矩形并注入body页面中
+  function createRectInBody() {
+    const rect = document.createElement('div');
+    rect.classList.add('rect');
+    document.body.appendChild(rect);
+    return rect;
+  }
+
+  // 确定元素起始位置
+  function ensureInitialPosition(element, x, y) {
+    element.style.left = `${x}px`;
+    element.style.top = `${y}px`;
+  }
+})();
